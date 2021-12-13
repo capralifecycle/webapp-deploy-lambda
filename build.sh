db642b93ca248b694cecc8ccf36dec1626bbf7e8
stekern <stekern@users.noreply.github.com>
2021-12-13 16:34:15 +0100

    fix: build script should not fail if python cache dir is missing

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ set -eux -o pipefail
 mkdir dist
 
 cp -r webapp_deploy dist/
-[ -e dist/webapp_deploy/__pycache__ ] && rm dist/webapp_deploy/__pycache__ -rf
+if [ -e dist/webapp_deploy/__pycache__ ]; then
+  rm dist/webapp_deploy/__pycache__ -rf
+fi
 
 # We currently do not install requirements.txt as the
 # items are already present in lambda runtime.
